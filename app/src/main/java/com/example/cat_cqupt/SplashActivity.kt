c8d92ad9f4762f2bd095e19caadcbdb096b88090
you <2719757561@qq.com>
2024-03-17 16:16:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cat_cqupt/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.cat_cqupt
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+
+/**
+ * TODO:Splash开机动画
+ */
+@SuppressLint("CustomSplashScreen")
+class SplashActivity: AppCompatActivity() {
+    private val SPLASH_DELAY_TIME = 2000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        delaySplash()
+    }
+
+    /**
+     * 延长开机动画
+     */
+    private fun delaySplash(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        },SPLASH_DELAY_TIME)
+    }
+}
